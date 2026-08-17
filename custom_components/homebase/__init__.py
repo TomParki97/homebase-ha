9e0be7be11afeeee1c4ae9fe6f788362afddb817
Tom Parkinson <tomparkinson.97@icloud.com>
2026-08-17 18:39:17 -0400

    Create initial HomeBase integration structure

---
+++ custom_components/homebase/__init__.py
@@ -0,0 +1,9 @@
+"""The HomeBase integration."""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up HomeBase."""
+    return True
---
+++ custom_components/homebase/const.py
@@ -0,0 +1,3 @@
+"""Constants for HomeBase."""
+
+DOMAIN = "homebase"
