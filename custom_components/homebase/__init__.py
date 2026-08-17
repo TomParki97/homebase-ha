16794c45d4ec7cb0e850e37f8fc40bcfd310ac8b
Tom Parkinson <tomparkinson.97@icloud.com>
2026-08-17 18:48:51 -0400

    Add HomeBase config flow

--- custom_components/homebase/__init__.py
+++ custom_components/homebase/__init__.py
@@ -1,9 +1,26 @@
 """The HomeBase integration."""
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up HomeBase."""
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Set up HomeBase from a config entry."""
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Unload a HomeBase config entry."""
     return True
